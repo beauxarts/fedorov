1f586702d9d4230ae63bc2fa93a0855cc99d0076
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-03-19 20:43:30 -0700

    Rolling back stencil prerender to fedorov

--- rest/post_prerender.go
+++ rest/post_prerender.go
@@ -1,8 +1,12 @@
 package rest
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/beauxarts/fedorov/stencil_app"
-	"github.com/boggydigital/stencil/stencil_rest"
+	"github.com/boggydigital/middleware"
+	"github.com/boggydigital/nod"
+	"io"
 	"net/http"
 )
 
@@ -26,5 +30,40 @@ func PostPrerender(w http.ResponseWriter, r *http.Request) {
 		paths = append(paths, sp)
 	}
 
-	stencil_rest.Prerender(paths, port, w)
+	// we don't want to accumulate existing static content over the lifetime of the app
+	middleware.ClearStaticContent()
+
+	host := fmt.Sprintf("http://localhost:%d", port)
+
+	for _, p := range paths {
+		if err := setStaticContent(host, p); err != nil {
+			http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	if _, err := io.WriteString(w, "ok"); err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
+func setStaticContent(host, p string) error {
+	resp, err := http.DefaultClient.Get(host + p)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	bs := make([]byte, 0, 1024*1024)
+	bb := bytes.NewBuffer(bs)
+
+	if _, err := io.Copy(bb, resp.Body); err != nil {
+		return err
+	}
+
+	middleware.SetStaticContent(p, bb.Bytes())
+
+	return nil
 }
