b5dac73b8226ec89179e29ef885ac7200d59d48f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-02-10 19:29:52 -0800

    Fixing cascade cmd to use batch write operation

--- cli/cascade.go
+++ cli/cascade.go
@@ -12,13 +12,12 @@ func CascadeHandler(_ *url.URL) error {
 	return Cascade()
 }
 
-// TODO: use batch operations
 func Cascade() error {
 
 	ca := nod.Begin("cascading reductions...")
 	defer ca.End()
 
-	props := []string{data.TitleProperty, data.BookCompletedProperty, data.ArtsHistoryOrderProperty}
+	props := []string{data.TitleProperty, data.BookCompletedProperty}
 
 	absReduxDir, err := pasu.GetAbsRelDir(data.Redux)
 	if err != nil {
@@ -30,26 +29,42 @@ func Cascade() error {
 		return ca.EndWithError(err)
 	}
 
-	// cascading data.BookCompletedProperty
-	bcpa := nod.NewProgress(" " + data.BookCompletedProperty)
-	defer bcpa.End()
-
-	ids := rdx.Keys(data.TitleProperty)
-	bcpa.TotalInt(len(ids))
-
-	for _, id := range ids {
-		bcpa.Increment()
-		if val, ok := rdx.GetFirstVal(data.BookCompletedProperty, id); ok && val != "" {
-			continue
-		}
-		if err := rdx.ReplaceValues(data.BookCompletedProperty, id, "false"); err != nil {
-			return ca.EndWithError(err)
-		}
+	if err := cascadeBookCompletedProperty(rdx); err != nil {
+		return ca.EndWithError(err)
 	}
 
-	bcpa.EndWithResult("done")
-
 	ca.EndWithResult("done")
+
+	return nil
+}
+
+func cascadeBookCompletedProperty(rdx kvas.WriteableRedux) error {
+
+	bca := nod.NewProgress(" " + data.BookCompletedProperty)
+	defer bca.End()
+
+	if err := rdx.MustHave(data.TitleProperty, data.BookCompletedProperty); err != nil {
+		return bca.EndWithError(err)
+	}
+
+	ids := rdx.Keys(data.TitleProperty)
+	bca.TotalInt(len(ids))
+
+	completed := make(map[string][]string)
+
+	for _, id := range ids {
+		bca.Increment()
+		if val, ok := rdx.GetFirstVal(data.BookCompletedProperty, id); ok && val != "" {
+			completed[id] = []string{"true"}
+		}
+		completed[id] = []string{"false"}
+	}
+
+	if err := rdx.BatchReplaceValues(data.BookCompletedProperty, completed); err != nil {
+		return bca.EndWithError(err)
+	}
+
+	bca.EndWithResult("done")
 
 	return nil
 }
