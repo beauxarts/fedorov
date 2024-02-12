7ac157056a259aafdadaa7ffa5fdd3fd7c98c11f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-02-11 21:06:49 -0800

    Adding basic frontend to download new books

---
+++ rest/get_latest.go
@@ -0,0 +1,83 @@
+package rest
+
+import (
+	"github.com/beauxarts/fedorov/data"
+	"github.com/boggydigital/kvas"
+	"github.com/boggydigital/nod"
+	"net/http"
+)
+
+type LatestBookViewModel struct {
+	Id      string
+	Title   string
+	Authors []string
+}
+
+func GetLatest(w http.ResponseWriter, r *http.Request) {
+
+	// GET /latest
+
+	var err error
+	if rdx, err = rdx.RefreshReader(); err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
+
+	lbvm := make([]*LatestBookViewModel, 0, latestBooksLimit)
+
+	if ahop, ok := rdx.GetAllValues(data.ArtsHistoryOrderProperty, data.ArtsHistoryOrderProperty); ok {
+		for _, id := range ahop[:latestBooksLimit] {
+			title := ""
+			if t, ok := rdx.GetLastVal(data.TitleProperty, id); ok {
+				title = t
+			}
+
+			var authors []string
+			if aus, err := authorsFullNames(id, rdx); err == nil {
+				authors = aus
+			}
+
+			lbvm = append(lbvm, &LatestBookViewModel{
+				Id:      id,
+				Title:   title,
+				Authors: authors,
+			})
+		}
+	}
+
+	DefaultHeaders(w)
+
+	if err := tmpl.ExecuteTemplate(w, "latest", lbvm); err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func authorsFullNames(id string, rdx kvas.ReadableRedux) ([]string, error) {
+
+	if err := rdx.MustHave(
+		data.PersonsIdsProperty,
+		data.PersonsRolesProperty,
+		data.PersonFullNameProperty,
+	); err != nil {
+		return nil, err
+	}
+
+	authorsNames := make([]string, 0)
+
+	if pids, ok := rdx.GetAllValues(data.PersonsIdsProperty, id); ok && len(pids) > 0 {
+		if prs, sure := rdx.GetAllValues(data.PersonsRolesProperty, id); sure && len(prs) == len(pids) {
+
+			for i := 0; i < len(prs); i++ {
+				if prs[i] != "author" {
+					continue
+				}
+				if afn, fine := rdx.GetLastVal(data.PersonFullNameProperty, pids[i]); fine {
+					authorsNames = append(authorsNames, afn)
+				}
+			}
+		}
+	}
+
+	return authorsNames, nil
+}
--- rest/routing.go
+++ rest/routing.go
@@ -23,24 +23,30 @@ func HandleFuncs(p int) {
 	port = p
 
 	patternHandlers := map[string]http.Handler{
-		// unauth data endpoints
-		"GET /books":       Log(http.HandlerFunc(GetBooks)),
-		"GET /book":        Log(http.HandlerFunc(GetBook)),
-		"GET /list_cover":  Log(http.HandlerFunc(GetListCover)),
-		"GET /book_cover":  Log(http.HandlerFunc(GetBookCover)),
-		"GET /search":      Log(http.HandlerFunc(GetSearch)),
-		"GET /digest":      Log(http.HandlerFunc(GetDigest)),
-		"GET /downloads":   Log(http.HandlerFunc(GetDownloads)),
-		"GET /description": Log(http.HandlerFunc(GetDescription)),
-		// auth data endpoints
-		"GET /completed/set":    Auth(Log(http.HandlerFunc(GetCompletedSet)), AdminRole),
-		"GET /completed/clear":  Auth(Log(http.HandlerFunc(GetCompletedClear)), AdminRole),
-		"GET /local-tags/edit":  Auth(Log(http.HandlerFunc(GetLocalTagsEdit)), AdminRole),
-		"GET /local-tags/apply": Auth(Log(http.HandlerFunc(GetLocalTagsApply)), AdminRole),
-		// auth media endpoints
-		"GET /file": Auth(Log(http.HandlerFunc(GetFile)), AdminRole, SharedRole),
+		//// unauth data endpoints
+		//"GET /books":       Log(http.HandlerFunc(GetBooks)),
+		//"GET /book":        Log(http.HandlerFunc(GetBook)),
+		//"GET /list_cover":  Log(http.HandlerFunc(GetListCover)),
+		//"GET /book_cover":  Log(http.HandlerFunc(GetBookCover)),
+		//"GET /search":      Log(http.HandlerFunc(GetSearch)),
+		//"GET /digest":      Log(http.HandlerFunc(GetDigest)),
+		//"GET /downloads":   Log(http.HandlerFunc(GetDownloads)),
+		//"GET /description": Log(http.HandlerFunc(GetDescription)),
+		//// auth data endpoints
+		//"GET /completed/set":    Auth(Log(http.HandlerFunc(GetCompletedSet)), AdminRole),
+		//"GET /completed/clear":  Auth(Log(http.HandlerFunc(GetCompletedClear)), AdminRole),
+		//"GET /local-tags/edit":  Auth(Log(http.HandlerFunc(GetLocalTagsEdit)), AdminRole),
+		//"GET /local-tags/apply": Auth(Log(http.HandlerFunc(GetLocalTagsApply)), AdminRole),
+		//// auth media endpoints
+		//"GET /file": Auth(Log(http.HandlerFunc(GetFile)), AdminRole, SharedRole),
 		// start at the books
-		"/": http.RedirectHandler("/books", http.StatusPermanentRedirect),
+		//"/": http.RedirectHandler("/books", http.StatusPermanentRedirect),
+
+		"GET /latest":   Log(http.HandlerFunc(GetLatest)),
+		"GET /new_book": Log(http.HandlerFunc(GetNewBook)),
+		"GET /file":     Auth(Log(http.HandlerFunc(GetFile)), AdminRole, SharedRole),
+
+		"/": http.RedirectHandler("/latest", http.StatusPermanentRedirect),
 	}
 
 	for p, h := range patternHandlers {
