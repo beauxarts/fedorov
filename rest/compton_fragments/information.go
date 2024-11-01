54f8d07cf6aafafacc0b9366f7ace4ecee12c42e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-10-31 21:51:53 -0700

    Fixing information and external links opening in iframes

--- rest/compton_fragments/information.go
+++ rest/compton_fragments/information.go
@@ -248,7 +248,9 @@ func propertyTitleValues(r compton.Registrar, property string, fmtProperty forma
 	}
 
 	tv := compton.TitleValues(r, compton_data.PropertyTitles[property]).
-		ForegroundColor(color.Gray).TitleForegroundColor(color.Foreground)
+		SetLinksTarget(compton.LinkTargetTop).
+		ForegroundColor(color.Gray).
+		TitleForegroundColor(color.Foreground)
 
 	if len(fmtProperty.values) > 0 {
 
