d68544aeda0399677c3e3057260bef39908379c1
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-01-09 11:13:20 -0800

    Bumping issa

--- rest/compton_fragments/set_tint.go
+++ rest/compton_fragments/set_tint.go
@@ -3,11 +3,12 @@ package compton_fragments
 import (
 	"github.com/beauxarts/fedorov/data"
 	"github.com/boggydigital/compton"
+	"github.com/boggydigital/issa"
 	"github.com/boggydigital/kevlar"
 )
 
 func SetTint(id string, p compton.Element, rdx kevlar.ReadableRedux) {
-	if repColor, ok := rdx.GetLastVal(data.RepItemImageColorProperty, id); ok {
+	if repColor, ok := rdx.GetLastVal(data.RepItemImageColorProperty, id); ok && repColor != issa.NeutralRepColor {
 		p.SetAttribute("style", "background-color:color-mix(in display-p3,"+repColor+" var(--cma),var(--c-background))")
 	}
 }
