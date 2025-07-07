7250eb4eabf2919bd0e4c150ad0a342873b93593
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-07-06 22:33:31 -0700

    Using more available horizontal space for menu, while preserving sections

--- rest/compton_data/search_scopes.go
+++ rest/compton_data/search_scopes.go
@@ -9,14 +9,12 @@ import (
 )
 
 var (
-	SearchNew   = "Новый"
 	SearchText  = litres_integration.ArtTypeText.String()
 	SearchAudio = litres_integration.ArtTypeAudio.String()
 	SearchPDF   = litres_integration.ArtTypePDF.String()
 )
 
 var SearchOrder = []string{
-	SearchNew,
 	SearchText,
 	SearchAudio,
 	SearchPDF,
@@ -24,8 +22,6 @@ var SearchOrder = []string{
 
 func SearchScopes() map[string]string {
 	queries := make(map[string]string, len(SearchOrder))
-
-	queries[SearchNew] = ""
 
 	q := url.Values{}
 	q.Set(data.ArtTypeProperty, strconv.Itoa(int(litres_integration.ArtTypeText)))
@@ -60,7 +56,7 @@ func EncodeQuery(query map[string][]string) string {
 func SearchScopeFromQuery(query map[string][]string) string {
 	enq := EncodeQuery(query)
 
-	searchScope := SearchNew
+	searchScope := ""
 	for st, sq := range SearchScopes() {
 		if sq == enq {
 			searchScope = st
